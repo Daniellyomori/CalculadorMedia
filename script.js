c7b69cd65f3df36a3ae0b794ef503c6799ddd59d
Danielly Omori Antunes de Oliveira <50346193+Daniellyomori@users.noreply.github.com>
2021-09-28 20:11:37 -0300

    Commit inicial

---
+++ script.js
@@ -0,0 +1,16 @@
+var nome = "Danielly"
+var notaPrimeiroBimestre = 9.834
+var notaSegundoBimestre = 7.034
+var notaTerceiroBimestre = 8.273
+var notaQuartoBimestre = 4.9283
+
+var notaFinal = (notaPrimeiroBimestre + notaSegundoBimestre + notaTerceiroBimestre + notaQuartoBimestre)/4
+
+var notaFixada = notaFinal.toFixed(1)
+
+console.log("Bem vindo "+nome)
+console.log("Nota Final:  "+ notaFixada)
+
+//Revisão
+//Variaveis: Lugares na memória do computador para referenciar um valor
+//string, console.log, toFixed, operacoes matematicas, concatenacao
